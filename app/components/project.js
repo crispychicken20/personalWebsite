9a2f842c2249708612912d2d61fd08168e7f4fb3
crispychicken20 <cperez29@student.ohlone.edu>
2025-10-03 21:38:43 -0700

    Some changes and additions

--- app/components/project.js
+++ app/components/project.js
@@ -1,10 +1,14 @@
 export default function Project(){
     return (
         <div className="p-8">
-            <h2 className="text-2x1 py-8 pb-4 font-bold"> Projects</h2>
-            <div>
-            <p>Project name</p>
-            <p>Project description goes here</p>
+            <h2 className="text-2x1 py-8 pb-2 font-bold"> Projects:</h2>
+            <div className="flex gap-40">
+            <h2 className="text-2x1 py-1">Project name</h2>
+            <h2 className="text-2x1 py-1">T.A.R.S.</h2>
+            </div>
+            <div className="flex gap-30">
+            <p>project description</p>
+            <p>project description</p>
             </div>
         </div>
         
