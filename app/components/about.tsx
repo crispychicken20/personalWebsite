e7a71aa81f845c9d4f07154a0c014a4009edb16d
crispychicken20 <cperez29@student.ohlone.edu>
2026-09-25 13:17:11 -0700

    Fix unescaped apostrophe in Ab out

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -3,7 +3,7 @@ export default function About() {
         <div id="about" className="scroll-mt-20">
             <h2 className="text-4xl pt-8 pb-4 font-bold text-center">About Me</h2>
             <p className="rounded-3xl font-3xl bg-green p-8 text-xl">
-                Hello! I'm Chris and I am a Data Scientist who is passionate and looking to break open into the industry!!  
+                Hello! I&apos;m Chris and I am a Data Scientist who is passionate and looking to break open into the industry!!  
             </p>
         </div>
     )
